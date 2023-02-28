7d45d366b408a566486f243c7e0133de458bc580
apicciano <antonio.picciano@datalogic.com>
2023-02-28 14:11:53 +0100

    Added simple com port example

--- java-com-port-terminal/src/main/java/org/example/javaComPortTerminal.java
+++ java-com-port-terminal/src/main/java/org/example/javaComPortTerminal.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import com.fazecast.jSerialComm.SerialPort;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -12,6 +13,23 @@ public class javaComPortTerminal extends Application {
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
+
+        SerialPort comPort = SerialPort.getCommPorts()[0];
+        comPort.openPort();
+        
+        try {
+            while (true)
+            {
+                while (comPort.bytesAvailable() == 0)
+                    Thread.sleep(20);
+
+                byte[] readBuffer = new byte[comPort.bytesAvailable()];
+                int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+                System.out.println("Read " + numRead + " bytes.");
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+        comPort.closePort();
+
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         Scene scene = new Scene(new StackPane(l), 640, 480);
         stage.setScene(scene);
