94d6c502db9e6425b45c3c8bd9ed39bc291e1310
apicciano <antonio.picciano@datalogic.com>
2023-02-28 14:50:01 +0100

    Added simple example to get all present com ports

--- java-com-port-terminal/src/main/java/org/example/javaComPortTerminal.java
+++ java-com-port-terminal/src/main/java/org/example/javaComPortTerminal.java
@@ -7,16 +7,18 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class javaComPortTerminal extends Application {
 
     @Override
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-
+/*
         SerialPort comPort = SerialPort.getCommPorts()[0];
         comPort.openPort();
-        
+
         try {
             while (true)
             {
@@ -29,12 +31,27 @@ public class javaComPortTerminal extends Application {
             }
         } catch (Exception e) { e.printStackTrace(); }
         comPort.closePort();
-
+*/
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Label comPorts = new Label();
+        for(String comPortName : getPortNames()) {
+            comPorts.setText(comPorts.getText() + " " + comPortName);
+        }
+
+        Scene scene = new Scene(new StackPane(comPorts), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
+
+    public static String[] getPortNames() {
+        SerialPort[] ports = SerialPort.getCommPorts();
+        ArrayList<String> portNames = new ArrayList<>();
+        for (SerialPort port : ports) {
+            portNames.add(port.getSystemPortName());
+        }
+        return portNames.toArray(new String[] {});
+    }
+
 
     public static void main(String[] args) {
         launch();
